82fccfb7a5ca6f17c0faa457b01d62bbe5336b73
lichuanjun <lichuanjun@niwodai.net>
2019-08-14 13:46:46 +0800

    修改app/routes里面的文件名称

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const app = express()
 const bodyParser = require('body-parser')
 
 // 引入路由
-const routes = require('./app/routes/routes')
-// 注册路由
-routes(app)
+const routes = require('./app/routes')
+
 
 // 配置静态文件
 app.use(express.static(path.join(__dirname, 'app/public')))
@@ -17,6 +16,9 @@ app.use('/apidoc', express.static(path.join(__dirname, 'app/public/apidoc/')))
 // 请求体解析中间件
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+// 注册路由
+routes(app)
 
 app.listen(3000)
 console.log('express-blog server started on: ' + 3000)
