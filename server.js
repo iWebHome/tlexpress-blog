c03be16869bda573038225a166f155613954f046
lichuanjun <lichuanjun@niwodai.net>
2019-08-14 16:42:11 +0800

    配置nodemon热加载服务的插件

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
+const log4js = require('log4js')
 
 // 引入路由
 const routes = require('./app/routes')
 
+let logger = log4js.getLogger()
+logger.level = 'debug'
+global.logger = logger
 
 // 配置静态文件
 app.use(express.static(path.join(__dirname, 'app/public')))
@@ -21,4 +25,6 @@ app.use(bodyParser.json())
 routes(app)
 
 app.listen(3000)
-console.log('express-blog server started on: ' + 3000)
+
+logger.info(`start:port is 3000`)
+// console.log('express-blog server started on: ' + 3000)
