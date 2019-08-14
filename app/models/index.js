7df8759e70d94d622311ffe9746034559aae15ac
lichuanjun <lichuanjun@niwodai.net>
2019-08-14 19:53:06 +0800

    添加单元测试模块，并实现测试服务的代码

--- app/models/index.js
+++ app/models/index.js
@@ -2,9 +2,9 @@
 
 const mdbs = {}
 mdbs.User = require('./users')
-// mdbs.Role = require('./role')
-// mdbs.Right = require('./right')
-// mdbs.Article = require('./article')
-// mdbs.ArticleCategory = require('./category')
+mdbs.Role = require('./role')
+mdbs.Right = require('./right')
+mdbs.Article = require('./article')
+mdbs.ArticleCategory = require('./category')
 
 module.exports = mdbs
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 const log4js = require('log4js')
+const mongoose = require('mongoose')
+
 const { logConfig, settings } = require('./config')
 
 // 引入路由
@@ -23,8 +25,17 @@ app.use('/apidoc', express.static(path.join(__dirname, 'app/public/apidoc/')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// 连接数据库
+// 将mongoose自身的promise替代为ES6的promise
+mongoose.Promise = global.Promise
+// MongoDB升级到4.0之后，需要加useNewUrlParser参数和useCreateIndex参数
+mongoose.connect(settings.dbConfig.URL, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.set('debug', settings.mongooseDebug)
+
 // 注册路由
 routes(app)
 
 app.listen(settings.port)
 logger.info(`start:port is ${settings.port}`)
+
+module.exports = app
